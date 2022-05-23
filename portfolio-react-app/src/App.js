d9ef305884d45af40110233ad06547dcf2700a60
unknown <avrumiesafranovitz@gmail.com>
2022-05-23 00:00:59 -0400

    updated portfolio section and contact

--- portfolio-react-app/src/App.js
+++ portfolio-react-app/src/App.js
@@ -1,6 +1,7 @@
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Work from "./components/Work";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar></Navbar>
       <Home></Home>
       <Work></Work>
+      <Contact></Contact>
     </div>
   );
 }
--- portfolio-react-app/src/components/Work.jsx
+++ portfolio-react-app/src/components/Work.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import WorkImg from '../assets/run-buddy.PNG'
+import Project from '../assets/group-project2.PNG'
+import TimeTracker from '../assets/work-day-scheduler.PNG'
+import RandomPassword from '../assets/random-password-generator.PNG'
 
 const Work = () => {
   return (
-    <div></div>
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-purple-700'>Work</p>
+                <p className='py-6'>Some recent work or mine</p>
+            </div>
+
+            <div 
+            className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+                
+                <div>
+                <a href="https://asafran92.github.io/run-buddy/">
+                    <img src={WorkImg} alt="RunBuddy" />
+                </a>
+
+                </div>
+                <div>
+                <a href="https://cb-travel-project.herokuapp.com/">
+                    <img src={Project} alt="Project2" />
+                </a>
+
+                </div>
+                <div>
+                <a href="https://asafran92.github.io/my-moments/">
+                    <img src={TimeTracker} alt="TimeTracker" />
+                </a>
+
+                </div>
+                <div>
+                <img src={RandomPassword} alt="RandomPassword" />
+
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
