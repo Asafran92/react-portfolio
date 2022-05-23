75b407fb52165db3c11689b871f8e76eddd23474
unknown <avrumiesafranovitz@gmail.com>
2022-05-23 00:28:20 -0400

    updated readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Professional Portfolio
+
+## Description
+
+Using React, create a professional portfolio to showcase my work.
+
+## Testing Instructions
+
+Once on the site, a user can click on a the nav bar header to be taken to a section of the site. The navbar uses "react smooth scrolling" for a nice user experience.
+
+## Screenshot og app in use:
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-        <form method='POST' action="https://getform.io/f/ec864726-2ede-48f1-861d-1c2b981f1f9f" classname='flex flex-col max-w[600px] w-full'>
+        <form method='POST' action="https://getform.io/f/ec864726-2ede-48f1-861d-1c2b981f1f9f" classname='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-purple-700 text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>Feel free to submit the form below, or send me an email at test@123.com</p>
