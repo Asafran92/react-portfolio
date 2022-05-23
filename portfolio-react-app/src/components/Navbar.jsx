296e9b356ab9bb136070cfb20bafd7b4a1927d75
unknown <avrumiesafranovitz@gmail.com>
2022-05-23 00:11:20 -0400

    updated navbar to include smooth scrolling

--- portfolio-react-app/src/components/Navbar.jsx
+++ portfolio-react-app/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
 import {HiOutlineMail} from 'react-icons/hi'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
 import Logo from '../assets/AS.png'
+import {Link} from 'react-scroll'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false)
@@ -15,10 +16,21 @@ const Navbar = () => {
       </div>
       {/* menu */}
         <ul className='hidden md:flex'>
-          <li>Home</li>
-          <li>About</li>
-          <li>Work</li>
-          <li>Contact</li>
+          <li>
+          <Link to="home" smooth={true} duration={500}>
+          Home
+        </Link>
+          </li>
+            <li>
+            <Link to="work" smooth={true} duration={500}>
+          Work
+        </Link>
+        </li>
+          <li>
+            <Link to="contact" smooth={true} duration={500}>
+          Contact
+        </Link>
+        </li>
         </ul>
 
       {/* hamburger */}
@@ -28,7 +40,6 @@ const Navbar = () => {
     {/* mobile menu */}
     <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
           <li className='py-6 text-4xl'>Home</li>
-          <li className='py-6 text-4xl'>About</li>
           <li className='py-6 text-4xl'>Work</li>
           <li className='py-6 text-4xl'>Contact</li>
         </ul>
